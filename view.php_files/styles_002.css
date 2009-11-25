6297cb9f6912f7d6c3120cfc7cd638a6129ea07e
Thiago R. Colucci <thiagorcolucci@agilbits.com.br>
2009-11-25 08:33:55 -0200

    enunciado e nossa primeira ontologia

---
+++ view.php_files/styles_002.css
@@ -0,0 +1,31 @@
+/**************************************
+ * THEME NAME: standardblue
+ *
+ * Files included in this sheet:
+ *
+ *   standardblue/gradients.css
+ **************************************/
+
+/***** standardblue/gradients.css start *****/
+
+/**
+  *  Adds all the nice finish to the standard theme
+  *
+  */
+
+th.header,
+td.header,
+div.header {     
+    background-image:url(gradient.jpg);     
+    background-position:top;    
+    background-repeat:repeat-x;     
+}
+
+.navbar {
+    background-image:url(gradient.jpg);     
+    background-position:top;    
+    background-repeat:repeat-x;     
+}
+
+/***** standardblue/gradients.css end *****/
+
